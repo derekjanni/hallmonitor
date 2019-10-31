6b02f06fa26253a0166c86e1a488714f760f988d
derekjanni <derekjanni@gmail.com>
2019-10-30 17:01:43 -0700

    add addon support

--- hallmonitor/addons.py
+++ hallmonitor/addons.py
@@ -1,8 +1,31 @@
-def get_auth0_token(url=None, audience=None, client_id=None, client_secret=None):
+import os
+import requests
+
+def auth0_handler(auth0_config):
+    token = get_auth0_token(
+        url=auth0_config['url'],
+        audience=auth0_config['audience'],
+        grant_type=auth0_config['grant_type'],
+        client_id=get_envar(auth0_config['client_id']),
+        client_secret=get_envar(auth0_config['client_secret'])
+    )
+    return {'Authorization': f'Bearer {token}'}
+
+def get_envar(name):
     """
-    Auth0 token is required for many API calls
+    Given an envar named ${ENVAR}, look for ENVAR in the local environment and return
+    """
+    envar = os.environ.get(name.strip('${}'))
+    if not envar:
+        raise Error(f'No such environment variable to load! {name}')
+    return envar
+
+def get_auth0_token(url=None, audience=None, grant_type=None, client_id=None, client_secret=None):
+    """
+    Get auth0 token
     """
     payload = f"audience={audience}&grant_type={grant_type}&client_id={client_id}&client_secret={client_secret}"
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    print(payload)
     response = requests.post(url, data=payload, headers=headers)
     return response.json()['access_token']
--- hallmonitor/hallmonitor.py
+++ hallmonitor/hallmonitor.py
@@ -1,14 +1,52 @@
-#!/usr/bin/env python
-
+import utils
 import yaml
 import shutil
 import argparse
 import requests
 import functools
+import utils
+import traceback
 import logging
+import addons
 from colorama import Fore, Back, Style
 
 logging.basicConfig(level=logging.DEBUG)
+
+# Use globals to inject headers. Follow the example for Auth0 header
+global AUTH_HEADER
+AUTH_HEADER=None
+
+# Define custom actions up here
+def export_auth_header(header):
+    global AUTH_HEADER
+    AUTH_HEADER=header
+
+ACTION_MAPPING = {
+    'auth_header': export_auth_header
+}
+
+# Define custom addons here
+ADDON_MAPPING = {
+    'auth0': addons.auth0_handler
+}
+
+def resolve_addon(addon):
+    """
+    Look I know this is evil but we parse the yaml and do what it says
+    to resolve addon functionality. Someday will have a better way
+    """
+    addon_name = addon['name']
+    addon_handler = ADDON_MAPPING[addon_name]
+    addon_action = addon['action']
+    result = addon_handler(addon)
+    action = ACTION_MAPPING[addon_action]
+    action(result)
+
+
+def save_response(filename, content):
+    with open(filename, 'wb') as f:
+        f.write(content)
+
 
 def handle_test():
     """
@@ -17,41 +55,62 @@ def handle_test():
     def _decorator(run_function):
         @functools.wraps(run_function)
         def _caller(*args, **kwargs):
-            # Check for cached response.
+            # Print header
             print(f'{Fore.WHITE}=' * shutil.get_terminal_size().columns)
+
+            # Print test description
             print(f'{Fore.YELLOW}{kwargs.get("description")}')
-            print(Style.RESET_ALL)
-            res = run_function(*args, **kwargs)
-            if res.status_code == 200:
-                print(f'{Fore.BLUE}{res}')
-                if kwargs.get('json'):
-                    print(f'{Fore.BLUE}{res.json()}')
 
-            else:
-                print(f'{Fore.RED}{res}')
-                print(f'{Fore.RED}{res.json()}')
-
+            # Reset style
             print(Style.RESET_ALL)
-            return res
+
+            try:
+                res = run_function(*args, **kwargs)
+                if res.status_code == 200:
+                    print(f'{Fore.BLUE}{res}')
+
+                    if kwargs.get('json'):
+                        print(f'{Fore.BLUE}{res.json()}')
+
+                    if kwargs.get('outfile'):
+                        save_response(kwargs.get('outfile'), res.content)
+                else:
+                    print(f'{Fore.RED}{res}')
+                    print(f'{Fore.RED}{res.json()}')
+
+                print(Style.RESET_ALL)
+            except:
+                print(f'{Fore.RED}')
+                traceback.print_exc()
+                print(Style.RESET_ALL)
 
         return _caller
     return _decorator
 
-@handle_test()
-def test_case(auth=None, **kwargs):
-    return requests.get(f'{kwargs["base"]}{kwargs["url"]}', params=kwargs.get('params'), headers=auth)
 
-if __name__ == '__main__':
+@handle_test()
+def test_case(**kwargs):
+    return requests.get(f'{kwargs["base"]}{kwargs["route"]}', params=kwargs.get('params'), headers=AUTH_HEADER)
+
+def main():
     # Load optional arguments
     parser = argparse.ArgumentParser()
-
-    parser.add_argument("--file", "-f", help="name of config file")
+    parser.add_argument("--path", "-p", default='', help="Path to config file")
+    parser.add_argument("--file", "-f", help="Name of config file")
     args = parser.parse_args()
     filename = args.file
+    path = args.path
 
-    with open(f'{filename}') as stream:
+    with open(f'{path}{filename}') as stream:
         config = yaml.safe_load(stream)
 
-    test_cases = config['test_cases']
+    addons = config.get('addons', [])
+    for addon in addons:
+        resolve_addon(addon)
+
+    test_cases = config.get('test_cases', [])
     for test in test_cases:
         test_case(**test)
+
+if __name__ == '__main__':
+    main()
